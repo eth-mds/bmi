2bacd552f06a20eb9b930f24aa282819c63d6cca
Drago Plecko <drago.plecko@stat.math.ethz.ch>
2021-08-10 17:38:07 +0200

    correct x-axis of missingess plot

--- scripts/summary-stats/graph-abstract-stats.R
+++ scripts/summary-stats/graph-abstract-stats.R
@@ -13,7 +13,7 @@ y_range <- c("death", "hypo", "tw_avg_glucose")
 res <- NULL
 for (y in y_range) {
   
-  ybin <- CI_dat(src, y = y, x_bins = c(18.5, 25), patient_ids = bmi)
+  ybin <- CI_dat(src, y = y, x_bins = c(18.5, 25, 30), patient_ids = bmi)
   ybin <- data.table::setnames(ybin, c("meanval", "V1"), c("Group", "Value"))
   if (is.element(y, c("death", "hypo"))) {
     ybin[, Value := 100 * Value]
